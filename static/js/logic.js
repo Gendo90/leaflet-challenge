1c7678d6397048fb50e3a72fba9d94207b9f408a
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-09-17 13:04:28 -0700

    feat: Add marker colors based on depth

--- static/js/logic.js
+++ static/js/logic.js
@@ -19,23 +19,30 @@ let USGS_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 let counter = 0;
 
+let colorCutoffDepths = [10, 30, 50, 70, 90]
+let colorMap = { 0: "00FFFF", 1: "00FF00", 2: "80FF00", 3: "FFFF00", 4: "FF8000", 5: "FF0000"};
+
 d3.json(USGS_url, function(data) {
     for (let i = 0; i < data.features.length; i++) {
         let curr_quake = data.features[i]
         let quake_time = new Date(curr_quake.properties["time"])
         let mag_scale = curr_quake.properties["mag"]
+        let quake_depth = curr_quake.geometry.coordinates[2];
+
+        let curr_color = colorMap[colorCutoffDepths.reduce((a, b, i) => b<quake_depth ? i+1 : a, 0)]
+
         let markerIcon = L.divIcon({
             html: `<svg width="${mag_scale * 10}" height="${mag_scale * 10}">
-                                <circle cx="${mag_scale * 5}" cy="${mag_scale * 5}" r="${mag_scale * 4}" stroke="black" stroke-width="1" fill="green" />
+                                <circle cx="${mag_scale * 5}" cy="${mag_scale * 5}" r="${mag_scale * 4}" stroke="black" stroke-width="1" fill=${"#"+curr_color} />
                                 </svg>`,
             iconAnchor: L.point([mag_scale * 5, mag_scale * 5]), 
             className: "circleIcon"
         });
 
         let curr_item = L.marker([curr_quake.geometry.coordinates[1], curr_quake.geometry.coordinates[0]], {icon: markerIcon})
-        curr_item.bindPopup("Magnitude: " + curr_quake.properties["mag"] +"<br>" + 
-                            "Depth: " + curr_quake.geometry.coordinates[2] + "<br>" +
-                            "Date: " + quake_time.toString());
+        curr_item.bindPopup("Magnitude: " + mag_scale +"<br>" + 
+                            "Depth: " + quake_depth + "<br>" +
+                            "Time: " + quake_time.toString());
 
         curr_item.addTo(myMap);
         counter++;
