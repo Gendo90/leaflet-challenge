7497076463d09ec0a60c6019b2646d0574f8f815
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-09-17 14:37:03 -0700

    feat: Add working legend box to show depths

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,6 +5,17 @@ let myMap = L.map("map", {
     worldCopyJump: true
 });
 
+// Create a legend to display information about our map
+var info = L.control({
+    position: "bottomright"
+});
+
+// When the layer control is added, insert a div with the class of "legend"
+info.onAdd = function () {
+    var div = L.DomUtil.create("div", "legend");
+    return div;
+};
+
 // Adding tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -13,7 +24,11 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: API_KEY
 }).addTo(myMap);
 
-//get geoJSON data and add markers to map
+// Add the info legend to the map
+info.addTo(myMap);
+
+
+// get geoJSON data and add markers to map
 
 let USGS_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
@@ -46,6 +61,78 @@ d3.json(USGS_url, function(data) {
 
         curr_item.addTo(myMap);
         counter++;
-        console.log(counter)
+        // console.log(counter)
     }
+
+    // now add the legend!
+    addLegend(colorCutoffDepths, colorMap)
 })
+
+// Add legend to map - function defined here
+function makeSwatch(color, size) {
+    let colorSwatch = document.createElement("span")
+    // colorSwatch.style.width = size + "px";
+    // colorSwatch.style.height = size + "px";
+    colorSwatch.setAttribute("width", size)
+    colorSwatch.setAttribute("height", size)
+    colorSwatch.style.backgroundColor = "#" + color;
+
+    let text = ""
+
+    for (let i = 0; i<size; i++) {
+        text += " "
+    }
+
+    colorSwatch.textContent = text;
+
+    // let rect = document.createElement("rect");
+    // // rect.style.width = size + "px";
+    // // rect.style.height = size + "px";
+    // rect.setAttribute("width", size)
+    // rect.setAttribute("height", size)
+    // rect.style.fill = "#"+color
+    // rect.style.rx = 0.2*size
+
+    // colorSwatch.appendChild(rect)
+
+    return colorSwatch
+
+}
+
+
+
+function addLegend(colorDepths, colors) {
+    let legend_el = document.querySelector(".legend");
+    let headerNote = document.createElement("h3")
+    headerNote.textContent = "Depths"
+    legend_el.appendChild(headerNote);
+
+    for (let i = 0; i < colorDepths.length; i++) {
+        let colorSwatch = makeSwatch(colors[i], 4)
+
+        //create text element to add
+        let curr_level = document.createElement("div")  
+
+        if(i === 0) {
+            curr_level.textContent += ` < ${colorDepths[0]}`
+            legend_el.appendChild(curr_level);
+        }
+        else {
+            curr_level.textContent = ` ${colorDepths[i - 1]} - ${colorDepths[i]}`
+            legend_el.appendChild(curr_level);
+        }
+
+        curr_level.prepend(colorSwatch);
+    }
+
+    // Add final color and largest value category
+    let lastInd = colorDepths.length
+    let colorSwatch = makeSwatch(colors[lastInd], 4)
+
+
+    let currLevel = document.createElement("div");
+    currLevel.textContent = ` > ${colorDepths[lastInd-1]}`
+    legend_el.appendChild(currLevel);
+    currLevel.prepend(colorSwatch)
+
+}
