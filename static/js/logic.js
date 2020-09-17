8fde593591eecc1def2d90d3ebea5a1a6335c8b3
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-09-17 11:47:24 -0700

    feat: Add functional map with markers to page

---
+++ static/js/logic.js
@@ -0,0 +1,35 @@
+// Creating map object
+let myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 8,
+    worldCopyJump: true
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+//get geoJSON data and add markers to map
+
+let USGS_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+let counter = 0;
+
+d3.json(USGS_url, function(data) {
+    for (let i = 0; i < data.features.length; i++) {
+        let curr_quake = data.features[i]
+        let quake_time = new Date(curr_quake.properties["time"])
+        let curr_item = L.marker([curr_quake.geometry.coordinates[1], curr_quake.geometry.coordinates[0]])
+        curr_item.bindPopup("Magnitude: " + curr_quake.properties["mag"] +"<br>" + 
+                            "Depth: " + curr_quake.geometry.coordinates[2] + "<br>" +
+                            "Date: " + quake_time.toString());
+
+        curr_item.addTo(myMap);
+        counter++;
+        console.log(counter)
+    }
+})
