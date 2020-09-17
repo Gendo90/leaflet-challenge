6015cfd3d1b148c6ea41516a38d09c4bdd37dfc3
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-09-17 12:29:51 -0700

    feat: Add custom SVG circle markers, with size denoting the magnitude of the quake

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,7 +23,16 @@ d3.json(USGS_url, function(data) {
     for (let i = 0; i < data.features.length; i++) {
         let curr_quake = data.features[i]
         let quake_time = new Date(curr_quake.properties["time"])
-        let curr_item = L.marker([curr_quake.geometry.coordinates[1], curr_quake.geometry.coordinates[0]])
+        let mag_scale = curr_quake.properties["mag"]
+        let markerIcon = L.divIcon({
+            html: `<svg width="${mag_scale * 10}" height="${mag_scale * 10}">
+                                <circle cx="${mag_scale * 5}" cy="${mag_scale * 5}" r="${mag_scale * 4}" stroke="black" stroke-width="1" fill="green" />
+                                </svg>`,
+            iconAnchor: L.point([mag_scale * 5, mag_scale * 5]), 
+            className: "circleIcon"
+        });
+
+        let curr_item = L.marker([curr_quake.geometry.coordinates[1], curr_quake.geometry.coordinates[0]], {icon: markerIcon})
         curr_item.bindPopup("Magnitude: " + curr_quake.properties["mag"] +"<br>" + 
                             "Depth: " + curr_quake.geometry.coordinates[2] + "<br>" +
                             "Date: " + quake_time.toString());
